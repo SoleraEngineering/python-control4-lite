2aac21e3e3b4ea7f0d26f6e0c67460ad1be111d8
Aleksi Asikainen <aleksi.asikainen@solera.com>
2018-08-03 16:36:36 -0500

    Fixed dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import io
 from setuptools import setup
 
-version = '0.1.1'
+version = '0.1.2'
 
 setup(
     name='python-control4-lite',
@@ -15,9 +15,10 @@ setup(
     author_email='aleksi.asikainen@solera.com',
     url='https://github.com/r3pi/python-control4-lite',
     packages=['control4',],
-    install_requires=['aiohttp>=3.3.2',],
+    install_requires=['aiohttp>=3.0.0',],
     license='MIT',
     long_description=io.open('README.txt', encoding='UTF-8').read(),
     test_suite='nose.collector',
-    tests_require=['nose']
+    tests_require=['nose'],
+    python_requires='>=3.6.0'
 )
